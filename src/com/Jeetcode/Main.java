127a627cf9447790f3d0b4e335fd4b9f8a5fa112
Limunard0 <josip.merda@gmail.com>
2022-01-30 09:48:43 +0100

    Finished task

--- src/com/Jeetcode/Main.java
+++ src/com/Jeetcode/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         int x = 133331; // x = 1221 = 1221, -121 = 121-
-
+// float, double = decimale,  char =  slova, asci tablica
         if (x < 0) {
             System.out.println("Broj je negativan. Nije paliendrom!");
         // ALGORITAM - put koji dovodi do rješenja -> primjer sa čajem!
@@ -25,16 +25,13 @@ public class Main {
 
     boolean isPalinedrome = true;
     for(int i = 0; i < lista.size() / 2; i++){
-        if(lista.get(1) != lista.get(lista.size() -1 -1)){
+        if(lista.get(i) != lista.get(lista.size() -i -1)){
             isPalinedrome = false;
 
         }
     }
-    if (isPalinedrome = false) System.out.println("Nije paliendrom");
+    if (isPalinedrome == false) System.out.println("Nije paliendrom");
     else System.out.println("Paliendrom je" );
-
-
-
 
     }
 }
